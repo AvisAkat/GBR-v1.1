465036797f61e27a2abc3102ba475b28f5603361
selasiii <52832049+selasiii@users.noreply.github.com>
2024-01-17 13:52:53 +0000

    migrations
    users, roles, staff, and routes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('user_id')->primary();
+            $table->foreignUuiduuid('role_id');
+            $table->foreignUuid('role_id')->references('role_id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('address');
+            $table->string('phone_number');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('gender');
+            $table->date('date_of_birth');
             $table->timestamps();
         });
     }
