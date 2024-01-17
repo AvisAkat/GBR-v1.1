cde44bdf9ffaedc2e624cfa1b611b36d0cad1dda
Mathias-Finney <mathiascharlesfinney@gmail.com>
2024-01-17 13:51:00 +0000

    passenger, buses and tickets tables created

---
+++ database/migrations/2024_01_17_124815_create-passenger-table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('passengers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('ticket_id')->references('id')->on('tickets')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignUuid('travel_id')->references('id')->on('travels')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('passengers');
+    }
+};
