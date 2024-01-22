1ae02f64a7db151fe2ea3dbf662d0cf365afaa6e
Mathias-Finney <mathiascharlesfinney@gmail.com>
2024-01-22 10:44:48 +0000

    just pushing

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->Uuid('user_id')->primary();
+            $table->uuid('user_id')->primary();
             $table->enum('role',['user','agent','admin'])->default('user');
             $table->string('first_name');
             $table->string('last_name');
